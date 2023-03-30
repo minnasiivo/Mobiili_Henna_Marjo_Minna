8cb18bf5774a73fe66e9a6829c62189d0094cef9
Marjo Jäkärä <marjo9o@hotmail.com>
2023-03-30 12:53:33 +0300

    laa-di-daa-daa

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Weather_app',
+      title: 'Sääsovellus',
       
       debugShowCheckedModeBanner: false, //Debug-bannerin piilotus
       theme: ThemeData(
