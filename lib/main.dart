d687f0e7dba4924dc3ad795e8770a2e540d8279b
Minna S <minna.siivo@student.lab.fi>
2023-03-31 10:01:51 +0300

    firebase tunnistus lisätty

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,49 @@ import 'package:flutter/material.dart';
 
 import 'package:weather_app/view/weather_page.dart';
 
-void main() => runApp(MyApp());
+import 'package:firebase_core/firebase_core.dart';
+
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:weather_app/view/weather_page.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var providers = [EmailAuthProvider()];
+
     return MaterialApp(
-      title: 'Weather App',
-      
-      debugShowCheckedModeBanner: false, //Debug-bannerin piilotus
-      theme: ThemeData(
-        primarySwatch: Colors.amber, //color: Color(0xffffb56b) MIKSI EI TOIMI?
-      ),
-      home: WeatherPage(),
+      debugShowCheckedModeBanner: false,
+      initialRoute:
+          FirebaseAuth.instance.currentUser == null ? '/sign-in' : '/mainpage',
+      routes: {
+        '/sign-in': (context) {
+          return SignInScreen(
+            providers: providers,
+            actions: [
+              AuthStateChangeAction<SignedIn>((context, state) {
+                Navigator.pushReplacementNamed(context, '/mainpage');
+              }),
+            ],
+          );
+        },
+        '/mainpage': (context) {
+          return WeatherPage();
+        },
+      },
     );
   }
 }
